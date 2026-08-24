43d63ddadafbc74bc34771d6b3b0ffbb33fac5de
Noura Manassra <116906900+NManassra@users.noreply.github.com>
2026-08-24 20:23:51 +0300

    chore: expose curated source from project archive

---
+++ src/Full_Adder_Sub_1212359.v
@@ -0,0 +1,9 @@
+module Full_Adder_Sub_1212359 #(parameter n=4)
+(input signed [n:0] x,y,
+output reg signed [n+1 : 0] result); //signed used to deal with negative and positive numbers by default and the size n since the overflow can be showen 
+always @(* )//since we have to use the behavoural model
+  begin 
+   result = x - y ;  
+  end 
+  
+endmodule
